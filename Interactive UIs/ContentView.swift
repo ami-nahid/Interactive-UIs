2254d8a36c10c146fca2429503be1325a2089173
Ami <ami.m.nahid@gmail.com>
2025-07-23 14:51:32 -0400

    created an interactive button and user input

--- Interactive UIs/ContentView.swift
+++ Interactive UIs/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var name = ""
+    @State private var textTitle = "hello"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            
+            
+            VStack {
+                Text (textTitle)
+                Text ("this is my interactive UI 🎀")
+                TextField("type your name here!", text: $name)
+                    .multilineTextAlignment(.center)
+                Button("click me!") {
+                    textTitle = "hey, \(name)"
+                    name = ""
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
