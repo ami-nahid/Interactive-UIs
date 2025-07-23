8ea6ceaaebadca3c914aaa2532ae9ddde99bbeb6
Ami <ami.m.nahid@gmail.com>
2025-07-23 14:28:10 -0400

    Initial Commit

---
+++ Interactive UIs/Interactive_UIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interactive_UIsApp.swift
+//  Interactive UIs
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Interactive_UIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
